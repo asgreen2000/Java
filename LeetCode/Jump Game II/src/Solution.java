35d9a185ec41b31c61d6d5e46e7e37e5000e086b
Bùi Hữu Tiến Đạt <dat.bui197404648@hcmut.edu.vn>
2021-06-21 19:56:56 +0700

    Add new comment

--- LeetCode/Jump Game II/src/Solution.java
+++ LeetCode/Jump Game II/src/Solution.java
@@ -5,21 +5,22 @@ public class Solution {
         
         int jumpToThis[] = new int[nums.length];
 
-        jumpToThis[0] = 0;
-
+        /* start jump from first index of the array */
+        jumpToThis[0] = 0; 
 
         for (int i = 1; i < nums.length; i++)
         {
             for (int j = 0; j < i; j++)
             {   
                 if (nums[j] + j >= i) {
-                    if (jumpToThis[i] == 0 || jumpToThis[i] < (jumpToThis[j] + 1))
+                    if (jumpToThis[i] == 0 || jumpToThis[i] < (jumpToThis[j] + 1)) 
                     {
                         jumpToThis[i] = jumpToThis[j] + 1;
                     }
                 }
             }
         }
+        
         return jumpToThis[nums.length - 1];
     }
 
