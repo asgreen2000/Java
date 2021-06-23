3e2562211002a21c8646484227a51bfc338a3db4
Bùi Hữu Tiến Đạt <dat.bui197404648@hcmut.edu.vn>
2021-06-24 01:35:42 +0700

    Change on Solution.java file

--- LeetCode/Validate Binary Search Tree/src/Solution.java
+++ LeetCode/Validate Binary Search Tree/src/Solution.java
@@ -1,4 +1,5 @@
-
+import java.util.ArrayList;
+import java.util.List;
 
 class TreeNode {
     int val;
@@ -14,7 +15,30 @@ class TreeNode {
 }
  
 public class Solution {
+
+    List<Integer> list = new ArrayList<>();
     public boolean isValidBST(TreeNode root) {
         
+        inOrder(root);
+        return isSorted();
+    }
+
+    boolean isSorted()
+    {
+        for (int i = 0; i < list.size() - 1; i++)
+        {
+            if (list.get(i + 1) < list.get(i))
+                return false;
+        }
+
+        return true;
+    }
+    void inOrder(TreeNode root) {
+
+        if (root == null)
+            return;
+        inOrder(root.left);
+        list.add(root.val);
+        inOrder(root.right);
     }
 }
